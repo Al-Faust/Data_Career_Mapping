3d88c12ec961d1cd5c20c486bdf7d4e1503a8030
Samatha Madur <madur_r@yahoo.com>
2020-09-02 19:15:17 -0500

    adding initial version

---
+++ run_scrape.py
@@ -0,0 +1,15 @@
+import glassdoor_scrape as gs
+import pandas as pd
+
+path = r"C:/Users/madur/Documents/Data_Analytics_Bootcamp/Homework/Project2/chromedriver"
+
+# scrape data scientist jobs
+#ds_df = gs.get_jobs('data scientist',1000, False, path, 15)
+
+# convert the data frame to csv
+#ds_df.to_csv('datas_scientist_jobs.csv', index = False)
+
+# scrape data analyst jobs
+da_df = gs.get_jobs('data scientist', 1000, False, path, 15)
+
+da_df.to_csv('ds.csv', index=False)
